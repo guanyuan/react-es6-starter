ae556617d3f96e2ab4da91b38e36704fae65c774
guanyuan <yuanguan89@gmail.com>
2016-11-01 00:03:53 +0800

    handle key press

--- src/FilterableProductTable/components/SearchBar.js
+++ src/FilterableProductTable/components/SearchBar.js
@@ -2,6 +2,29 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
+  componentDidMount() {
+    this.refs.filterTextInput.addEventListener('keydown', this.handleKeyPress)
+  }
+
+  handleKeyPress = e => {
+    let index = 0
+    console.log(e.code)
+    console.log(typeof (e.code))
+    switch(e.code) {
+      case 'ArrowDown':
+        //arrow-down
+        index = index + 1
+        break
+      case 'ArrowUp':
+        //arrow-up
+        index = index -1
+        break
+      default:
+        break
+    }
+    console.log(index)
+  }
+
   handleChange = () => {
     this.props.onUserInput(
       this.refs.filterTextInput.value,
